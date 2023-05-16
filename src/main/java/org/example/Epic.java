a0814ce0e53a0df56c49653c2d478d61d7b21cc6
Oleg <ovt1966@gmail.com>
2023-05-16 14:29:42 +0400

    Add Clas

---
+++ src/main/java/org/example/Epic.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+}
+
